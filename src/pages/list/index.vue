b58e61e2133f48d6e1a6a24093e07fc557c6b8a9
Radu Rentea <radurentea@Mariuss-MacBook-Pro.local>
2019-10-25 14:55:37 +0300

    Add filter functionality based on color and models fetched from the API

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,8 +1,39 @@
 <template>
-    <div class="q-pa-md">
-    <q-card >
+  <div class="q-pa-md">
+    <q-card>
       <q-card-section>
         <div class="text-h6">List of cars</div>
+
+        <div class="q-gutter-md q-my-md row">
+          <q-select
+            filled
+            v-model="color"
+            label="Color"
+            :options="this.car.colors"
+            style="width: 250px; "
+          >
+            <template v-slot:no-option>
+              <q-item>
+                <q-item-section class="text-grey">No results</q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+
+          <q-select
+            filled
+            v-model="model"
+            label="Model"
+            :options="this.car.models"
+            style="width: 250px;"
+          >
+            <template v-slot:no-option>
+              <q-item>
+                <q-item-section class="text-grey">No results</q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+          <q-btn color="primary" icon="filter_list" label="Filter" @click="filterList" />
+        </div>
       </q-card-section>
 
       <q-markup-table>
@@ -14,51 +45,93 @@
           </tr>
         </thead>
         <tbody>
-          <CarItem v-for="car in car.cars" :key="car.id" :car="car"/>
+          <CarItem v-for="car in car.cars" :key="car.id" :car="car" />
         </tbody>
-         
       </q-markup-table>
     </q-card>
-    <div class="text-center q-pt-md pagination"> 
-      <template  v-if="page != 1">
-        <router-link :to="{ name: 'car-list', query: { page: page - 1 } }" rel="prev">
-        Prev Page</router-link>
-      <template v-if="hasNextPage"> | </template>
+    <div class="text-center q-pt-md pagination">
+      <template v-if="page != 1">
+        <router-link :to="{ name: 'car-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link>
+        <template v-if="hasNextPage">|</template>
       </template>
-      <router-link v-if="hasNextPage" :to="{ name: 'car-list', query: { page: page + 1 } }" rel="next">
-        Next Page</router-link>
+      <router-link
+        v-if="hasNextPage"
+        :to="{ name: 'car-list', query: { page: page + 1 } }"
+        rel="next"
+      >Next Page</router-link>
     </div>
   </div>
 </template>
 
 <script>
-import CarItem from '../../components/CarItem.vue'
-import { mapState } from 'vuex'
+import CarItem from "../../components/CarItem.vue";
+import { mapState } from "vuex";
 
-    export default {
-        components: {
-          CarItem
-        },
-        created() {
-          this.$store.dispatch('car/fetchCars', {
-            page: this.page,
-            pageSize:  this.car.pageSize
-          })
-        },
-        computed: {
-          page() {
-            return parseInt(this.$route.query.page) || 1
-          },
-          hasNextPage() {
-            return this.car.carsTotal > this.page * this.car.pageSize
-          },
-          ...mapState(['car'])
-        }
+export default {
+  components: {
+    CarItem
+  },
+  data() {
+    return {
+      color: null,
+      model: null
+    };
+  },
+  methods: {
+    // filterByColor(val, update, abort) {
+    //   update(() => {
+    //     const needle = val.toLowerCase();
+    //     this.options = colorOptions.filter(
+    //       v => v.toLowerCase().indexOf(needle) > -1
+    //     );
+    //     //add code here for query
+    //   });
+    // },
+    // filterByModel(val, update, abort) {
+    //   update(() => {
+    //     const needle = val.toLowerCase();
+    //     this.options = modelOptions.filter(
+    //       v => v.toLowerCase().indexOf(needle) > -1
+    //     );
+    //   });
+    // },
+    filterList() {
+      this.$store
+        .dispatch("car/fetchCars", {
+          page: this.page,
+          pageSize: this.car.pageSize,
+          color: this.color,
+          model: this.model
+        })
+        .then(() => {
+          this.color = "";
+          this.model = "";
+        });
     }
+  },
+  created() {
+    this.$store.dispatch("car/fetchCars", {
+      page: this.page,
+      pageSize: this.car.pageSize
+    });
+
+    this.$store.dispatch("car/fetchColors");
+    this.$store.dispatch("car/fetchModels");
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1;
+    },
+    hasNextPage() {
+      return this.car.carsTotal > this.page * this.car.pageSize;
+    },
+    ...mapState(["car"])
+  }
+};
 </script>
 
 <style scoped>
-.pagination a{
+.pagination a {
   text-decoration: none;
   font-size: 1rem;
 }
