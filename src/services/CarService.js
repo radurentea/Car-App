0facc2bb669b2de1744032492f92aa9e0feaa703
Radu Rentea <radurentea@Mariuss-MacBook-Pro.local>
2019-10-25 14:57:49 +0300

    Edit getCars to take color and model as parameter and Add getColors and getModels functions

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -1,28 +1,44 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000/api`,
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+	baseURL: `http://localhost:3000/api`,
+	withCredentials: false, // This is the default
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json'
+	}
+});
 
 export default {
-  getCars(page, pageSize) {
-    return apiClient.get('/cars?page=' + page + '&pageSize=' + pageSize)
-  },
-  getCar(id) {
-    return apiClient.get('/cars/' + id)
-  },
-  postCar(car) {
-    return apiClient.post('/cars', car)
-  },
-  putCar(car) {
-    return apiClient.put(`/cars/${encodeURIComponent(car.id)}`,  car)
-  },
-  deleteCar(car) {
-    return apiClient.delete(`/cars/${encodeURIComponent(car.id)}`, car)
-  }
-}
+	getCars(page, pageSize, color, model) {
+		if (!color && !model) {
+			return apiClient.get('/cars?page=' + page + '&pageSize=' + pageSize);
+		} else if (color && model) {
+			return apiClient.get(
+				'/cars?page=' + page + '&pageSize=' + pageSize + '&color=' + color + '&model=' + model
+			);
+		} else if (color) {
+			return apiClient.get('/cars?page=' + page + '&pageSize=' + pageSize + '&color=' + color);
+		} else if (model) {
+			return apiClient.get('/cars?page=' + page + '&pageSize=' + pageSize + '&model=' + model);
+		}
+	},
+	getCar(id) {
+		return apiClient.get('/cars/' + id);
+	},
+	postCar(car) {
+		return apiClient.post('/cars', car);
+	},
+	putCar(car) {
+		return apiClient.put(`/cars/${encodeURIComponent(car.id)}`, car);
+	},
+	deleteCar(car) {
+		return apiClient.delete(`/cars/${encodeURIComponent(car.id)}`, car);
+	},
+	getColors() {
+		return apiClient.get('/colors');
+	},
+	getModels() {
+		return apiClient.get('/models');
+	}
+};
